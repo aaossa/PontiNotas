acff694714d264b2c786a74feffd60821025fbce
Antonio Ossa <aaossa@uc.cl>
2017-01-06 01:48:13 -0300

    Improved demo

--- demo.js
+++ demo.js
@@ -14,99 +14,37 @@ var gradesData = querystring.stringify({
     tprt: 'FAA'
 });
 
-var optionsGetLogin = {
-    hostname: parsedLoginUrl.hostname,
-    port: (parsedLoginUrl.port || 443), // 80 => http, 443 => https
-    method: 'GET',
-    path: parsedLoginUrl.path,
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
-    },
-};
-var optionsPostLogin = {
-    hostname: parsedLoginUrl.hostname,
-    port: (parsedLoginUrl.port || 443),
-    method: 'POST',
-    path: parsedLoginUrl.path,
-    headers: {
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(loginData),
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
-    },
-};
-var optionsPostGrades = {
-    hostname: parsedGradesUrl.hostname,
-    port: (parsedGradesUrl.port || 443),
-    method: 'POST',
-    path: parsedGradesUrl.path,
-    headers: {
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(gradesData),
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
-    },
-};
-
-const getLogin = function(options) {
+function postRequest(parsedUrl, qsData, cookies) {
     return new Promise((resolve, reject) => {
+        var options = {
+            hostname: parsedUrl.hostname,
+            port: (parsedUrl.port || 443),
+            method: 'POST',
+            path: parsedUrl.path,
+            headers: {
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(qsData),
+                'Cookie': cookies,
+            },
+        };
         const request = https.request(options, (response) => {
-            console.log('>> Set-Cookie (getLogin): ' + response.headers['set-cookie']);
             const body = [];
             response.on('data', (chunk) => body.push(chunk));
-            response.on('end', () => resolve(response.headers));
+            response.on('end', () => resolve([response.headers, body.join('')]));
         });
         request.on('error', (err) => reject(err));
-        request.end();
-    });
-};
-const postLogin = function(options, cookies) {
-    return new Promise((resolve, reject) => {
-        options['headers']['Cookie'] = cookies;
-        const request = https.request(options, (response) => {
-            console.log('>> Enviada (postLogin): ' + options['headers']['Cookie']);
-            console.log('>> Recibida (postLogin): ' + response.headers['set-cookie']);
-            const body = [];
-            response.on('data', (chunk) => body.push(chunk));
-            response.on('end', () => resolve(response.headers));
-        });
-        request.on('error', (err) => reject(err));
-        request.write(loginData);
-        request.end();
-    });
-};
-const postGrades = function(options, cookies) {
-    return new Promise((resolve, reject) => {
-        options['headers']['Cookie'] = cookies;
-        const request = https.request(options, (response) => {
-            console.log('>> Enviada (postLogin): ' + options['headers']['Cookie']);
-            console.log('>> Recibida (postGrades): ' + response.headers['set-cookie']);
-            console.log(response.statusCode);
-            const body = [];
-            response.on('data', (chunk) => body.push(chunk));
-            response.on('end', () => resolve(body.join(''), response.headers)); // resolve(body.join('')));
-        });
-        request.on('error', (err) => reject(err));
-        request.write(gradesData);
+        request.write(qsData);
         request.end();
     });
 };
 
-getLogin(optionsGetLogin)
-    .then(function(loginHeaders) {
-        console.log(loginHeaders);
-        postLogin(optionsPostLogin, loginHeaders['set-cookie'])
-            .then(function(gradesGetHeaders) {
-                console.log();
-                console.log(gradesGetHeaders);
-                postGrades(optionsPostGrades, gradesGetHeaders['set-cookie'])
-                    .then(function(body, gradesPostHeaders) {
-                        console.log();
-                        console.log(body);
-                        console.log(gradesPostHeaders);
-                    })
-                    .catch((err) => console.error(err));
-            })
-            .catch((err) => console.log(err));
+var cookies = 'TESTID=set,SESSID=; expires=Mon, 01-Jan-1990 08:00:00 GMT';
+postRequest(parsedLoginUrl, loginData, cookies)
+    .then(([headers, body]) => {
+        return postRequest(parsedGradesUrl, gradesData, headers['set-cookie']);
     })
-    .catch((err) => console.log(err));
+    .then(([headers, body]) => {
+        console.log(headers);
+    })
+    .catch(console.log.bind(console));
